2581b5f2214874cac1e299645daba18023e477a8
Justin <justinjdbennett@gmail.com>
2023-01-25 20:12:40 -0700

    Show totals for products and categories in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PASS']
 
-  def show; end
+  def show
+    @products = Product.count
+    @categories = Product.distinct.pluck(:category_id).count
+  end
 end
