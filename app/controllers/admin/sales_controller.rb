c235a60b69d3cc9017e7d18933ee7b404bc1d7a6
Justin <justinjdbennett@gmail.com>
2023-01-30 20:11:39 -0700

    Add basic_auth to admin/sales

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,4 +1,6 @@
 class Admin::SalesController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PASS']
+
   def index
     @sales = Sale.all.order(ends_on: :desc)
   end
