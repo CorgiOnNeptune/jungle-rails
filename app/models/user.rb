bc1653be859d0540e4a8e0307f7817e2147b0bfa
Justin <justinjdbennett@gmail.com>
2023-01-31 01:14:03 -0700

    Add password length validation and email unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
-  validates :password_digest, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password_confirmation, presence: true, on: :create
 end
